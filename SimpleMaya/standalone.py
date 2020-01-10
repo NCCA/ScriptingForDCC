61bc531177bd4ed89020dd824b797cacdda84626
jmacey <jmacey@bournemouth.ac.uk>
2020-01-10 08:56:08 +0000

    updated the maya standalone bpython init files

--- SimpleMaya/standalone.py
+++ SimpleMaya/standalone.py
@@ -1,2 +1,7 @@
+from __future__ import print_function
+import os
 import maya.standalone
 maya.standalone.initialize(name='python')
+os.system('clear')
+print('Maya Python Standalone Terminal initialized')
+
