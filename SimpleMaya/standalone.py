c129b58541dabfb111f8414e0b5f2e784578910b
jmacey <jmacey@bournemouth.ac.uk>
2020-01-10 11:53:48 +0000

    added exit handler to bpython demo

--- SimpleMaya/standalone.py
+++ SimpleMaya/standalone.py
@@ -1,7 +1,14 @@
 from __future__ import print_function
-import os
+import os,atexit
 import maya.standalone
+
+def exitMaya() :
+    print('closing down maya')
+    maya.standalone.uninitialize()
+
+atexit.register(exitMaya)
 maya.standalone.initialize(name='python')
 os.system('clear')
 print('Maya Python Standalone Terminal initialized')
+
 
