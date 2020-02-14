6c034a32130f6e6b216e1f0440a1505b9185c350
jmacey <jmacey@bournemouth.ac.uk>
2020-02-14 09:15:02 +0000

    updates to rigging tools

--- Rigging/CreateControls.py
+++ Rigging/CreateControls.py
@@ -2,14 +2,38 @@ from __future__ import print_function,division
 import maya.cmds as cmds
 import maya.api.OpenMaya as om
 
+  
 
-dag=om.MItDag( filterType=om.MFn.kJoint)
-dagFN=om.MFnDagNode()
-# grab the selected items 
-sel=om.MGlobal.getActiveSelectionList() 
-print(sel)
-while not dag.isDone() :
-  node=dag.currentItem()
-  dagFN.setObject(node) 
-  print(' found joint called {} '.format(dagFN.name()))
-  dag.next() # move to next node
+def main() :
+  grpPostfix='CtlGrp'
+  controlPostfix='ctrl'
+  dag=om.MItDag( filterType=om.MFn.kJoint)
+  dagFN=om.MFnDagNode()
+  # grab the selected items 
+  sel=om.MGlobal.getActiveSelectionList()
+  firstNode=True 
+  while not dag.isDone() :
+    node=dag.currentItem()
+    dagFN.setObject(node) 
+    ctrlName='{}{}'.format(dagFN.name(),controlPostfix)
+    print(' found joint called {} '.format(dagFN.name()))
+    if firstNode == True :
+      parentName='{}{}'.format(dagFN.name(),grpPostfix)
+      print('FirstNode ',parentName)
+      cmds.group(em=True, name=parentName)
+      cmds.circle(name=ctrlName)
+      cmds.parent(ctrlName,parentName)
+      firstNode=False  
+    else :
+      print(' adding group',parentName)
+#      cmds.group(em=True, name='{}CTRL'.format(dagFN.name()))
+      cmds.group(em=True, name='{}{}'.format(dagFN.name(),grpPostfix), parent=parentName)
+      cmds.circle(name=ctrlName)
+      cmds.parent(ctrlName,parentName)
+      parentName='{}{}'.format(dagFN.name(),grpPostfix)
+
+    dag.next() # move to next node
+
+
+if __name__ == '__main__' :
+  main()
