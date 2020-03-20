b9f6691d08c3898b523db9051dda51e14ca12944
jmacey <jmacey@bournemouth.ac.uk>
2020-03-20 10:28:46 +0000

    almost working image write, need to make the default setting better

--- UnitTestMaya/MayaImage.py
+++ UnitTestMaya/MayaImage.py
@@ -1,11 +1,9 @@
-#!/Applications/Autodesk/maya2019/Maya.app/Contents/bin/mayapy
 import maya.api.OpenMaya as OpenMaya
 import ctypes
 
 
 class MayaImage :
-  @classmethod 
-  def __init__(self,filename=None,width=None,height=None,channels=4,imagetype=OpenMaya.kByte) :
+  def __init__(self,filename=None,width=None,height=None,channels=4,imagetype=OpenMaya.MImage.kByte) :
     """ constructor pass in the name of the file to load (absolute file name with path)
     or width and height to create and image
     """
@@ -29,6 +27,9 @@ class MayaImage :
       self.readImage(filename)
     else :
       self.image.create(self.width,self.height,self.channels,self.type)
+      self.charPixelPtr = ctypes.cast(self.image.pixels(), ctypes.POINTER(ctypes.c_char) )
+      self.width,self.height=self.image.getSize()
+      
 
   def readImage(self,filename) :
     # read from file MImage should handle errors for us so no need to check
@@ -36,13 +37,26 @@ class MayaImage :
     self.width,self.height=self.image.getSize()
     # get the pixel data
     self.charPixelPtr = ctypes.cast(self.image.pixels(), ctypes.POINTER(ctypes.c_char) )
-
     # query to see if it's an RGB or RGBA image, this will be True or False
     self.hasAlpha=self.image.isRGBA()
-    # data is always packed as RGBA even if no alpha present
-    self.imgStep=4
 
+  def setPixel(self,x,y,r,g,b,a=255) :
+    # check the bounds to make sure we are in the correct area
+    if x<0 or x>self.width :
+      raise IndexError('x value out of range')
+    if y<0 or y>self.height :
+      raise IndexError('y value out of range')
+    # now calculate the index into the 1D array of data
+    index=(y*self.width*self.channels)+x*self.channels
+    # grab the pixels we need to convert from char to integer ordinal type so use ord
+    self.charPixelPtr[index]=ctypes.c_char(chr(r))
+    self.charPixelPtr[index+1]=ctypes.c_char(chr(g))
+    self.charPixelPtr[index+2]=ctypes.c_char(chr(b))
+    self.charPixelPtr[index+3]=ctypes.c_char(chr(a))
 
+  def save(self,filename,outputFormat='png') :
+    self.image.writeToFile(filename+'.'+outputFormat,outputFormat=outputFormat)
+    
   def getPixel(self,x,y) :
     """ get the pixel data at x,y and return a 3/4 tuple depending upon type """
     # check the bounds to make sure we are in the correct area
@@ -53,7 +67,7 @@ class MayaImage :
       print "error y our of bounds\n"
       return
     # now calculate the index into the 1D array of data
-    index=(y*self.width*4)+x*4
+    index=(y*self.width*self.channels)+x*self.channels
     # grab the pixels we need to convert from char to integer ordinal type so use ord
     red   = ord(self.charPixelPtr[index])
     green = ord(self.charPixelPtr[index+1])
--- UnitTestMaya/TestMayaImage.py
+++ UnitTestMaya/TestMayaImage.py
@@ -8,7 +8,7 @@ class TestMayaImage(unittest.TestCase):
   @classmethod
   def setUpClass(self):
     print('doing setup')
-    
+
   def testConstructFromWidthHeight(self) :
     image=mi.MayaImage(width=100,height=200)
     self.assertEqual(image.width,100)
@@ -17,6 +17,24 @@ class TestMayaImage(unittest.TestCase):
   def testThrowFromBadFilename(self) :
     with self.assertRaises(ValueError) :
       _=mi.MayaImage(200)
+  def testThrowFromIndexError(self) :
+    with self.assertRaises(IndexError) :
+      img=mi.MayaImage(width=10,height=10)
+      img.setPixel(200,100,255,255,0)
+  def testSaveImage(self) :
+    image=mi.MayaImage(width=100,height=100)
+    for h in range(0,100) :
+      for w in range(0,100) :
+        image.setPixel(w,h,255,0,0)
+    image.save("test","png")
+
+  def testOpenImage(self) :
+    image=mi.MayaImage("test.png")
+    r,g,b,_=image.getPixel(2,2)
+    self.assertEqual(r,255)
+    self.assertEqual(g,0)
+    self.assertEqual(b,0)
+    
 
 if __name__ == '__main__' :
   maya.standalone.initialize(name='python')
